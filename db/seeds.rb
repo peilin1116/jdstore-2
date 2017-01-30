18b99aa85984142daf05254c3b90c0b93344ec62
peilin <307644224@qq.com>
2017-01-30 23:12:56 +0800

    only admin can acess backend panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.new
+u.email = "peilin@peilin.com"
+
+u.password = "112233"
+
+u.password_confirmation = "112233"
+
+u.is_admin = true
+u.save
