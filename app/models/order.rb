ca2d3cabc85b8b1e1a54f80c8f59c89743df9337
peilin <307644224@qq.com>
2017-02-08 17:02:50 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+   belongs_to :users
+
+   validates :billding_name, presence: true
+   validates :billding_address, presence: true
+   validates :shipping_name, presence: true
+   validates :shipping_address, presence: true
+   
 end
