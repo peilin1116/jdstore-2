8c383d6096b5a275caa7260bddd227cadab01a6c
peilin <307644224@qq.com>
2017-02-08 22:23:20 +0800

    add product_list model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208084128) do
+ActiveRecord::Schema.define(version: 20170208141757) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -28,12 +28,21 @@ ActiveRecord::Schema.define(version: 20170208084128) do
   create_table "orders", force: :cascade do |t|
     t.integer  "total",            default: 0
     t.integer  "user_id"
-    t.string   "billding_name"
-    t.string   "billding_address"
+    t.string   "billing_name"
+    t.string   "billing_address"
     t.string   "shipping_name"
     t.string   "shipping_address"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+  end
+
+  create_table "product_lists", force: :cascade do |t|
+    t.integer  "order_id"
+    t.string   "product_name"
+    t.integer  "product_price"
+    t.integer  "quantity"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "products", force: :cascade do |t|
